78fb1cfc9a629b6ce283b577a1d98a12c7dfd5aa
jenjohmar <johannajennifer4@gmail.com>
2023-10-19 13:17:57 +0200

    first commit

---
+++ src/FirePokemon.java
@@ -0,0 +1,4 @@
+public class FirePokemon {
+    public FirePokemon() {
+    }
+}
---
+++ src/Pokemon.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public abstract class Pokemon {
+    private List<Pokemon> pokemons;
+    private int level;
+    private int hp;
+    public Pokemon() {
+    }
+
+    food;/*""*/
+    sound;/*""*/
+    town;/*""*/
+}
